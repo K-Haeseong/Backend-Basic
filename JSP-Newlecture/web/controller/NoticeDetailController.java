73cc4a945d07289e6de68f8e3fb73841d4aa4576
Haeseong Kim <113011771+K-Haeseong@users.noreply.github.com>
2023-04-11 23:57:44 +0900

    JSP를 이용한 자바 웹 프로그램 만들기
    * 자세한 페이지 수정하기
    * 목록에 댓글 수를  포함하기 위한 쿼리 문제
    * 목록의 댓글 수를 위한 view 생성하기

--- JSP-Newlecture/web/controller/NoticeDetailController.java
+++ JSP-Newlecture/web/controller/NoticeDetailController.java
@@ -15,6 +15,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.newlecture.web.entity.Notice;
+import com.newlecture.web.service.NoticeService;
 
 @WebServlet("/notice/detail")
 public class NoticeDetailController extends HttpServlet{
@@ -26,63 +27,11 @@ public class NoticeDetailController extends HttpServlet{
 		
 		int id =  Integer.parseInt(request.getParameter("id"));
 
-
-		String url = "jdbc:oracle:thin:@localhost:1521/xepdb1";
-		String sql = "SELECT * FROM NOTICE WHERE ID=?";
-
-
-		try {
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-			Connection con = DriverManager.getConnection(url,"newlec", "101800");
-			PreparedStatement st = con.prepareStatement(sql);
-			st.setInt(1, id);
-
-			ResultSet rs = st.executeQuery();
-
-			rs.next();
-
-			String title = rs.getString("TITLE");
-			String writer_id = rs.getString("WRITER_ID");
-			Date regdate = rs.getDate("REGDATE");
-			String hit = rs.getString("HIT");
-			String files = rs.getString("FILES");
-			String content = rs.getString("CONTENT");
-			
-			Notice notice = new Notice(
-					id,
-					title,
-					writer_id,
-					regdate,
-					hit,
-					files,
-					content
-				);
-					
-			request.setAttribute("n", notice); 
-			
-			/*
-			 * request.setAttribute("title", title); 
-			 * request.setAttribute("writer_id",writer_id); 
-			 * request.setAttribute("regdate", regdate);
-			 * request.setAttribute("hit", hit); request.setAttribute("files", files);
-			 * request.setAttribute("content", content);
-			 */
-
-			rs.close();
-			st.close();
-			con.close();
-			
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
+		NoticeService service = new NoticeService();
+		Notice notice = service.getNotice(id);
+		request.setAttribute("n", notice);
 		
 		// redirect => 여기서 다른 페이지로 가는 것 (로그인 안 되었으니까 로그인 하고 오라고 보내버릴 때)
-		
 		// forward => 여기서 작업했던 내용 이어 받아서 처리 할 때
 		
 		
