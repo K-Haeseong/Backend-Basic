83764432c745d11ce4e40b3b43fbf7b6d9a96aa5
Haeseong Kim <113011771+K-Haeseong@users.noreply.github.com>
2023-04-25 01:19:01 +0900

    파일 업로드 마무리
    * 파일 저장을 위한 물리 경로 얻기
    * 단일 파일 업로드
    * 다중 파일 업로드
    * 업로드 된 파일 사용하기

--- JSP-Newlecture/web/controller/admin/notice/RegController.java
+++ JSP-Newlecture/web/controller/admin/notice/RegController.java
@@ -1,16 +1,30 @@
 package com.newlecture.web.controller.admin.notice;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.InputStream;
+import java.util.Collection;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
 
 import com.newlecture.web.entity.Notice;
 import com.newlecture.web.service.NoticeService;
+
+@MultipartConfig(
+//		location = "/tmp", 
+//		일반적으로 설정 X why? =>	절대경로는 서비스를 실행하는 리눅스와 윈도우즈에 차이가 있으므로 
+//		차라리 설정을 안 하고 자바가 지정된 임시 디레토리를 사용하자.
+		fileSizeThreshold = 1024*1024,
+		maxFileSize = 1024*1024*50,
+		maxRequestSize = 1024*1024*50*5
+)
 
 @WebServlet("/admin/board/notice/reg")
 public class RegController extends HttpServlet {
@@ -28,8 +42,57 @@ public class RegController extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String title = request.getParameter("title");
+		
+		System.out.print("title : ");
+		System.out.println(title);
+		
 		String content = request.getParameter("content");
 		String isOpen = request.getParameter("open");
+		
+		
+		Collection<Part> parts = request.getParts();
+		StringBuilder builder = new StringBuilder();
+		for(Part p : parts) {
+			if(!p.getName().equals("file")) continue;
+			if(p.getSize() == 0 ) continue;
+			
+			Part filePart = p;
+			String fileName = filePart.getSubmittedFileName();
+			builder.append(fileName);
+			builder.append(",");
+			
+			InputStream fis = filePart.getInputStream();
+			
+			String realPath = request.getServletContext().getRealPath("/upload");
+			System.out.println(realPath);
+			
+			File path = new File(realPath);
+			if(!path.exists())
+				path.mkdirs();
+			
+			
+			
+			String filePath = realPath + File.separator + fileName;
+			FileOutputStream fos = new FileOutputStream(filePath);
+			
+			
+			byte[] buf = new byte[1024];
+			int size = 0;
+			while((size = fis.read(buf)) != -1)
+				fos.write(buf, 0, size);
+				
+			fos.close();
+			fis.close();
+			
+		}
+		
+		
+		
+		builder.delete(builder.length()-1, builder.length());
+			
+			
+		
+		
 		boolean pub = false;
 		if(isOpen != null)
 			pub = true;
@@ -39,6 +102,7 @@ public class RegController extends HttpServlet {
 		notice.setContent(content);
 		notice.setPub(pub);
 		notice.setWriter_id("newlec");
+		notice.setFiles(builder.toString());
 		
 		
 		NoticeService service = new NoticeService();
@@ -47,11 +111,6 @@ public class RegController extends HttpServlet {
 		response.sendRedirect("list");
 		
 		
-		
-		
 	}
-		
-
-
 
 }
