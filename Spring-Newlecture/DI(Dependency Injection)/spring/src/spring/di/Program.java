b29ab8ebafcd9585f385465722a85ae1d1d251f0
Haeseong Kim <113011771+K-Haeseong@users.noreply.github.com>
2023-05-02 23:40:23 +0900

    어노테이션을 이용한 DI

--- Spring-Newlecture/DI(Dependency Injection)/spring/src/spring/di/Program.java
+++ Spring-Newlecture/DI(Dependency Injection)/spring/src/spring/di/Program.java
@@ -10,7 +10,8 @@ public class Program {
 	public static void main(String[] args) {
 		 
 //		스프링에게 지시하는 방법으로 코드를 변경
-//		Exam exam = new NewlecExam(); 
+//		Exam exam = new NewlecExam();  
+//		Exam exam = new NewlecExam(10,10,10,10);  
 //		ExamConsole console = new GridExamConsole();
 //		
 //		console.setExam(exam);
@@ -21,9 +22,19 @@ public class Program {
 		ApplicationContext context = 
 				new ClassPathXmlApplicationContext("spring/di/setting.xml");
 		
+		//Exam exam = context.getBean(Exam.class);
+		//System.out.println(exam.toString());
 		ExamConsole console = (ExamConsole) context.getBean("console");
-//		ExamConsole console = context.getBean(ExamConsole.class);
+		//ExamConsole console = context.getBean(ExamConsole.class);
 		console.print();
+		
+		//List<Exam> exams = (List<Exam>) context.getBean("exams");  // new ArrayList<>();
+		//exams.add(new NewlecExam(1,1,1,1));
+		
+		
+		//for (Exam e : exams) {
+		//	System.out.println(e);
+		//}
 		
 		
 	}
