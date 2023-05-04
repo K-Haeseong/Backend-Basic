52a1ff741158d539e4623a680cb2787d0e615f9f
Haeseong Kim <113011771+K-Haeseong@users.noreply.github.com>
2023-05-04 23:53:17 +0900

    Java Configuration

--- Spring-Newlecture/DI(Dependency Injection)/spring/src/spring/di/ui/InlineExamConsole.java
+++ Spring-Newlecture/DI(Dependency Injection)/spring/src/spring/di/ui/InlineExamConsole.java
@@ -2,31 +2,41 @@ package spring.di.ui;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 
 import spring.di.entity.Exam;
 
+@Component("console")
 public class InlineExamConsole implements ExamConsole {
 
+	@Autowired
+	@Qualifier("exam2")
 	private Exam exam;
 	
+	
 	public InlineExamConsole() {
+		System.out.println("constructor");
 	}
 	
 	public InlineExamConsole(Exam exam) {
+		System.out.println("overloaeded constructor");
 		this.exam = exam;
 	}
 
 
 	@Override
 	public void print() {
+		if(exam == null)
+			System.out.printf("total is %d, av is %f\n", 0, 0.0);
+		else	
 		System.out.printf("total is %d, av is %f\n", exam.total(), exam.avg());
 	}
 
-	@Autowired
-	@Qualifier("exam2")
 	@Override
 	public void setExam(Exam exam) {
+		System.out.println("setter");
 		this.exam =exam;
 
 	}
+
 }
