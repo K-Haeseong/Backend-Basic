5427b27beab2961e3c5b60bb45961ecce50e9793
Haeseong Kim <113011771+K-Haeseong@users.noreply.github.com>
2023-05-11 19:11:51 +0900

    XML형식  Annotation으로 변경

--- Spring-Newlecture/MVC/src/java/com/newlecture/web/controller/IndexController.java
+++ Spring-Newlecture/MVC/src/java/com/newlecture/web/controller/IndexController.java
@@ -1,28 +1,27 @@
 package com.newlecture.web.controller;
 
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-public class IndexController implements Controller {
+@Controller
+public class IndexController  {
 
-	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		
-		System.out.println("index controller");
-		
-		// 기존방식
+	@RequestMapping("/index")
+	public String index() {
+		return "root.index";
+	}
+	
+//	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+//		
+// 		<기존방식>
 //		ModelAndView mv = new ModelAndView();
 //		mv.setViewName("index");
 //		mv.addObject("data", "Hello Spring MVC~!!");
 //		mv.setViewName("/WEB_INF/view/index.jsp");
 		
-		// 변경된방식
-		ModelAndView mv = new ModelAndView("root.index");
-		mv.addObject("data", "Hello Spring MVC~!!");
-		
-		
-		return mv;
-	}
+// 		<변경된방식>
+//		ModelAndView mv = new ModelAndView("root.index");
+//		mv.addObject("data", "Hello Spring MVC~!!");
+//		return mv;
 }
