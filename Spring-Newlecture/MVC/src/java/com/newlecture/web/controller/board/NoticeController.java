bcbaf8c39709b3efdd5ed8aeadff0e94b324d849
Haeseong Kim <113011771+K-Haeseong@users.noreply.github.com>
2023-05-16 23:12:19 +0900

    파일 업로드 설정

--- Spring-Newlecture/MVC/src/java/com/newlecture/web/controller/board/NoticeController.java
+++ Spring-Newlecture/MVC/src/java/com/newlecture/web/controller/board/NoticeController.java
@@ -3,12 +3,14 @@ package com.newlecture.web.controller.admin.board;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller("adminNoticeController")
-@RequestMapping("/admin/board/")
+@RequestMapping("/admin/board/notice/")
 public class NoticeController {
 	
 	@RequestMapping("list")
+	@ResponseBody
 	public String list() {
 		
 		return "list";
@@ -17,9 +19,20 @@ public class NoticeController {
 	
 	@RequestMapping("reg")
 	@ResponseBody
-	public String reg(String title, String content, String category, String[] foods, String food) {
-		return String.format("title : %s<br> content : %s<br> category : %s<br> foods : %s<br> food : %s<br>"
+	public String reg(String title, String content, String category, 
+			String[] foods, String food, MultipartFile file) {
+			
+			String fileName = file.getOriginalFilename();
+			long size = file.getSize();
+			
+			System.out.printf("fileName:%s, fileSize:%d\n", fileName, size);
+			
+			
+			return String.format("title : %s<br> content : %s<br> category : %s<br> "
+				+ "foods : %s<br> food : %s<br>"
 							, title, content, category, foods, food);
+			
+	
 	}
 	
 	@RequestMapping("edit")
