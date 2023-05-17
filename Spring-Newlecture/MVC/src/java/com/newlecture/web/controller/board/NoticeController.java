7875c08a32b4666f13f09c96515fd9e5f1944e63
Haeseong Kim <113011771+K-Haeseong@users.noreply.github.com>
2023-05-17 21:10:49 +0900

    관리자 페이지 설정
    * 물리경로 얻기, 파일저장
    * 다중 파일 업로드
    * post매핑과 redirection

--- Spring-Newlecture/MVC/src/java/com/newlecture/web/controller/board/NoticeController.java
+++ Spring-Newlecture/MVC/src/java/com/newlecture/web/controller/board/NoticeController.java
@@ -1,52 +1,70 @@
 package com.newlecture.web.controller.admin.board;
 
+import java.io.File;
+import java.io.IOException;
+
+import javax.servlet.ServletContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 @Controller("adminNoticeController")
 @RequestMapping("/admin/board/notice/")
 public class NoticeController {
 	
+	@Autowired
+	private ServletContext ctx;
+	
 	@RequestMapping("list")
-	@ResponseBody
 	public String list() {
 		
-		return "list";
+		return "admin.board.notice.list";
 	}
 
+	@GetMapping("reg")
+	public String reg() {
+		return "admin.board.notice.reg";
+	}
 	
-	@RequestMapping("reg")
-	@ResponseBody
+	@PostMapping("reg")
 	public String reg(String title, String content, String category, 
-			String[] foods, String food, MultipartFile file) {
+			String[] foods, String food, MultipartFile[] files) throws IllegalStateException, IOException {
 			
-			String fileName = file.getOriginalFilename();
-			long size = file.getSize();
+			for (MultipartFile file : files) {
+				String fileName = file.getOriginalFilename();
+				long size = file.getSize();
+				System.out.printf("fileName:%s, fileSize:%d\n", fileName, size);
+				
+				String webPath = "/static/upload";
+				String realPath = ctx.getRealPath(webPath);
+				System.out.printf("realPath : %s\n", realPath);
+				// 경로설정
+				File savePath = new File(realPath);
+				if(!savePath.exists())
+					savePath.mkdirs();
+				// 파일저장
+				realPath += File.separator + fileName;
+				File saveFile = new File(realPath);
+				file.transferTo(saveFile);
+			}
 			
-			System.out.printf("fileName:%s, fileSize:%d\n", fileName, size);
-			
-			
-			return String.format("title : %s<br> content : %s<br> category : %s<br> "
-				+ "foods : %s<br> food : %s<br>"
-							, title, content, category, foods, food);
-			
-	
+			return "admin.board.notice.list";
 	}
 	
 	@RequestMapping("edit")
-	@ResponseBody
 	public String edit() {
 		
-		return "edit";
+		return "admin.board.notice.edit";
 	}
 	
 	@RequestMapping("delete")
-	@ResponseBody
 	public String delete() {
 		
-		return "delete";
+		return "admin.board.notice.delete";
 	}
 	
 	
